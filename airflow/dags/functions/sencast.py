920f47064e9509dee5cc7534eecfd8164fe7e3a3
James Runnalls <runnalls.james@gmail.com>
2023-10-06 10:47:55 +0200

    Add even with zero pixels

--- airflow/dags/functions/sencast.py
+++ airflow/dags/functions/sencast.py
@@ -116,6 +116,13 @@ def create_sencast_operational_metadata(ds, **kwargs):
                                      "satellite": parts[-3], "parameter": parameter, "key": key, "pixels": pixels,
                                      "valid_pixels": valid_pixels, "prefix": "/".join(path[0:-1]),
                                      "file": file, "min": data_min, "max": data_max, "mean": data_mean})
+                            else:
+                                tiff_keys.append(
+                                    {"lake": lake["properties"]["Name"], "processor": parts[0],
+                                     "tile": parts[-1].split(".")[0], "datetime": parts[-2],
+                                     "satellite": parts[-3], "parameter": parameter, "key": key, "pixels": pixels,
+                                     "valid_pixels": valid_pixels, "prefix": "/".join(path[0:-1]),
+                                     "file": file})
                     except:
                         failed.append(file)
                         print("FAILED.")
@@ -132,7 +139,7 @@ def create_sencast_operational_metadata(ds, **kwargs):
         for parameter in parameters:
             for lake in lakes:
                 out = [{"dt": d["datetime"], "k": d["key"], "p": d["pixels"], "vp": d["valid_pixels"], "min": d["min"],
-                        "max": d["max"], "mean": d["mean"]} for d in tiff_keys if d['parameter'] == parameter and d["lake"] == lake]
+                        "max": d["max"], "mean": d["mean"]} for d in tiff_keys if d['parameter'] == parameter and d["lake"] == lake and d["valid_pixels"] > 0]
                 if len(out) > 0:
                     s3.put_object(
                         Body=json.dumps(out),
