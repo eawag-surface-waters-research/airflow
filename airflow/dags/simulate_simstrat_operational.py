c03ee06704d2d0aa084df72683d5b2e94aa22206
James Runnalls <runnalls.james@gmail.com>
2024-11-14 10:02:21 +0100

    Add space

--- airflow/dags/simulate_simstrat_operational.py
+++ airflow/dags/simulate_simstrat_operational.py
@@ -59,7 +59,7 @@ run_simulation = BashOperator(
     bash_command="mkdir -p {{ filesystem }}/git;"
                  "cd {{ filesystem }}/git;"
                  "git clone {{ simulation_repo_https }} && cd {{ simulation_repo_name }} || cd {{ simulation_repo_name }} && git stash && git pull;"
-                 "python src/main.py operational server_host={{ API_HOST }} sever_user={{ API_USER }} server_password={{ API_PASSWORD }} visualcrossing_key={{ VISUALCROSSING_KEY }}"
+                 "python src/main.py operational server_host={{ API_HOST }} sever_user={{ API_USER }} server_password={{ API_PASSWORD }} visualcrossing_key={{ VISUALCROSSING_KEY }} "
                  "overwrite_simulation={{ params.overwrite_simulation }} docker_dir={{ FILESYSTEM }}/git/{{ simulation_repo_name }}",
     on_failure_callback=report_failure,
     dag=dag,
