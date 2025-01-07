e244c9f1f97864aa488e94f3b925367155baade9
James Runnalls <runnalls.james@gmail.com>
2025-01-07 09:11:29 +0100

    Improve resiliance of myswitzerland parse

--- airflow/dags/current_water_temperature.py
+++ airflow/dags/current_water_temperature.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timezone, timedelta
 import os
 import json
+import time
 import pytz
 import boto3
+import random
 import requests
 import tempfile
 
@@ -277,45 +279,50 @@ def collect_water_temperature(ds, **kwargs):
         if response.status_code == 200:
             data = response.json()
             for station in data:
-                response = requests.get("https://sospo.myswitzerland.com/lakesides-swimming-pools/{}".format(station))
-                if response.status_code == 200:
-                    root = parse_html(response.text)
-                    element = html_find_all(root, tag="a", class_name="AreaMap--link")
-                    location = element[0].get('href').split("?q=")[-1].split(",")
-                    coords = [float(location[1]), float(location[0])]
-                    label = html_find_all(root, tag="h1", class_name="PageHeader--title")[0].text
-                    date = datetime.strptime(
-                        html_find_all(root, tag="div", class_name="QuickFactsWidget--info")[0].text.strip().split(": ",
-                                                                                                                  1)[1],
-                        "%d.%m.%Y, %H:%M")
-                    date = swiss_timezone.localize(date).timestamp()
-                    value = False
-                    icon = "lake"
-                    for info in html_find_all(root, tag="ul", class_name="QuickFacts--info"):
-                        c = html_find_all(info, tag="li", class_name="QuickFacts--content")
-                        if len(c) == 1:
-                            content = c[0].text
-                            value = html_find_all(info, tag="li", class_name="QuickFacts--value")[0].text
-                            if content in ["Lake bathing", "River pools"] and value != "—":
-                                if content == "River pools":
-                                    icon = "river"
-                                value = float(value.replace("°", ""))
-                    if value and date > min_date:
-                        features.append({
-                            "type": "Feature",
-                            "id": "myswitzerland_{}".format(station),
-                            "properties": {
-                                "label": label,
-                                "last_time": date,
-                                "last_value": value,
-                                "url": "https://sospo.myswitzerland.com/lakesides-swimming-pools/{}".format(station),
-                                "source": "MySwitzerland",
-                                "icon": icon,
-                                "lake": False
-                            },
-                            "geometry": {
-                                "coordinates": coords,
-                                "type": "Point"}})
+                try:
+                    time.sleep(random.uniform(0, 0.3))
+                    response = requests.get("https://sospo.myswitzerland.com/lakesides-swimming-pools/{}".format(station))
+                    if response.status_code == 200:
+                        root = parse_html(response.text)
+                        element = html_find_all(root, tag="a", class_name="AreaMap--link")
+                        location = element[0].get('href').split("?q=")[-1].split(",")
+                        coords = [float(location[1]), float(location[0])]
+                        label = html_find_all(root, tag="h1", class_name="PageHeader--title")[0].text
+                        date = datetime.strptime(
+                            html_find_all(root, tag="div", class_name="QuickFactsWidget--info")[0].text.strip().split(": ",
+                                                                                                                      1)[1],
+                            "%d.%m.%Y, %H:%M")
+                        date = swiss_timezone.localize(date).timestamp()
+                        value = False
+                        icon = "lake"
+                        for info in html_find_all(root, tag="ul", class_name="QuickFacts--info"):
+                            c = html_find_all(info, tag="li", class_name="QuickFacts--content")
+                            if len(c) == 1:
+                                content = c[0].text
+                                value = html_find_all(info, tag="li", class_name="QuickFacts--value")[0].text
+                                if content in ["Lake bathing", "River pools"] and value != "—":
+                                    if content == "River pools":
+                                        icon = "river"
+                                    value = float(value.replace("°", ""))
+                        if value and date > min_date:
+                            features.append({
+                                "type": "Feature",
+                                "id": "myswitzerland_{}".format(station),
+                                "properties": {
+                                    "label": label,
+                                    "last_time": date,
+                                    "last_value": value,
+                                    "url": "https://sospo.myswitzerland.com/lakesides-swimming-pools/{}".format(station),
+                                    "source": "MySwitzerland",
+                                    "icon": icon,
+                                    "lake": False
+                                },
+                                "geometry": {
+                                    "coordinates": coords,
+                                    "type": "Point"}})
+                except Exception as e:
+                    print(e)
+                    failed.append("MySwitzerland: "+station)
     except Exception as e:
         print(e)
         failed.append("MySwitzerland")
